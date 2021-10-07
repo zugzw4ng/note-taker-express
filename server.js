672775cd05a640e0edb113c2da257d2a33c81a39
zugzw4ng <bongcloud2001@gmail.com>
2021-10-07 14:10:59 -0400

    added static public pages on server

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
