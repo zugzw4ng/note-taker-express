6e4f880404419e76aaa30d74ea5331757cd02cde
zugzw4ng <bongcloud2001@gmail.com>
2021-10-08 17:45:25 -0400

    fixed ordering of get requests

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
 
 app.post("/api/notes", (req, res) => {
     let newNote = req.body;
