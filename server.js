1ae453fc7524c85831403fdb7ec98c883d2f33de
zugzw4ng <bongcloud2001@gmail.com>
2021-10-08 17:17:25 -0400

    added post functionality on backend

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 // * `GET *` should return the `index.html` file.
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 // The following API routes should be created:
 
 // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
@@ -22,3 +24,15 @@ app.get("/api/notes", (req, res) => {
 });
 
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteHistory = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+    newNote.id = notelength;
+    noteHistory.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+
+
